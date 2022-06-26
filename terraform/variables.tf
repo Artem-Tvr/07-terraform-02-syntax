54b4fbf59fd7df7fc638d0674e4114eb52a013ff
Artem_Budin <artyombudin@ya.ru>
2022-06-26 23:56:45 +0300

    26-06_2

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,17 +1,11 @@
-# Заменить на ID своего облака
-# https://console.cloud.yandex.ru/cloud?section=overview
 variable "yandex_cloud_id" {
   default = "b1gie5ilhq7pn8551q66"
 }
 
-# Заменить на Folder своего облака
-# https://console.cloud.yandex.ru/cloud?section=overview
 variable "yandex_folder_id" {
   default = "b1gjgsqvh4kc21t4nfs3"
 }
 
-# Заменить на ID своего образа
-# ID можно узнать с помощью команды yc compute image list
 variable "ubuntu-2004-lts" {
   default = "fd8f1tik9a7ap9ik2dg1"
 }
